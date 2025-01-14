d3da2869bf2e74cc7e486dcd508055cf607d7432
Nicholas-Fearon <nejfearon@gmail.com>
2025-01-14 15:44:18 +0000

    User logged in implemented

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -5,16 +5,16 @@ const { db } = require("@/utils/db");
 export default async function PostsPage() {
   const results = await db.query(`SELECT * FROM wedding_guestbook`);
   const posts = results.rows;
-  const {user} = await auth();
+  const user = await auth();
   console.log("This is my user log:", user);
   console.log(posts);
   return (
     <>
       <h1>Guest Posts</h1>
-      {user === null ? (
-        <Link href="/form">Write a message</Link>
+      {user.userId === null || undefined ? (
+       <Link href="/sign-in">Sign-in to make a post</Link> 
       ) : (
-        <Link href="/sign-in">Sign-in to make a post</Link>
+        <Link href="/form">Write a message</Link>
       )}
 
       {posts.map((post) => (
